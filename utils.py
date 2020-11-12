0a8029948a59b4c4f513ec3b75d91cc30ad9f77f
phungpx <phamxuanphungbku@gmail.com>
2020-11-12 14:18:38 +0700

    update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-import cv2
-from pathlib import Path 
+import yaml
+from pathlib import Path
 from importlib import import_module
 
+
 def load_yaml(yaml_file):
-	import yaml
-	with open(yaml_file) as f:
-		settings = yaml.safe_load(f)
-	return settings
+    with open(yaml_file) as f:
+        settings = yaml.safe_load(f)
+    return settings
+
 
 def create_instance(config, *args, **kwargs):
-    from importlib import import_module
     module = config['module']
     name = config['name']
     config_kwargs = config.get(name, {})
