bf51efc42f6414d4b247fa47f3d8b916b2740ab1
phungpx <phamxuanphungbku@gmail.com>
2021-09-18 22:04:01 +0700

    Update config, run

--- simple_copy_paste.py
+++ simple_copy_paste.py
@@ -53,9 +53,6 @@ class SimpleCopyPaste(BaseCopyPaste):
         # choose back ground image randomly
         bg_image = cv2.imread(str(random.choice(self.bg_images)))
 
-        # gaussian shadow for foreground image
-        # image = self.gaussian_shadow(image=image)
-
         # augment background
         for augmenter in random.sample(
             self.background_transforms,
@@ -127,52 +124,3 @@ class SimpleCopyPaste(BaseCopyPaste):
             image = augmenter(image=image)
 
         return image, label
-
-    def gaussian_shadow(self, image, center=(-1, -1), r=-1):
-        '''
-        Args:
-            image: image: H, W, 3, ndarray
-            center: (x,y); (-1,-1) for random
-            r: -1 for random
-        Outputs:
-            shadowed image
-        '''
-        x_center, y_enter = center
-        height, width = image.shape[:2]
-
-        if center == (-1, -1):
-            x_center = random.randint(0, width - 1)
-            y_center = random.randint(0, height - 1)
-        if r == -1:
-            r = random.randint(1000, 2000)
-
-        # Generate gaussian
-        sigma = r / 3.0
-        x = np.arange(0, 2 * r, 1, np.float32)
-        y = x[:, np.newaxis]
-        # The gaussian is not normalized, we want the center value to equal 1
-        g = np.exp(- ((x - r) ** 2 + (y - r) ** 2) / (2 * sigma ** 2))
-
-        mask = np.zeros(shape=(height, width))
-
-        x_min = max(x_center - r, 0)
-        x_max = min(x_center + r, width)
-        y_min = max(y_center - r, 0)
-        y_max = min(y_center + r, height)
-
-        g_remain = g[(r - (y_center - y_min)):(r + (y_max - y_center)),
-                     (r - (x_center - x_min)):(r + (x_max - x_center))]
-
-        for i in range(x_min, x_max):
-            for j in range(y_min, y_max):
-                mask[j, i] = g_remain[j - y_min, i - x_min]
-
-        mask = np.stack([mask] * 3, axis=-1)
-
-        # do em agment trên ảnh scan nên em chỉ làm tối đi thôi.
-        bright_image = iaa.MultiplyAndAddToBrightness(mul=(0.6, 1.5), add=(-40, 40))(image=image)
-
-        image = bright_image * mask + image * (1 - mask)
-        image = image.astype(np.uint8)
-
-        return image
